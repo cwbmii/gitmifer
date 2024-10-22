cd983f18b545cb442bf4bdadca0f34161516ab1a
cwbmii <117386724+cwbmii@users.noreply.github.com>
2024-10-22 11:51:55 +0000

    alteracoes

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+   'Programação',
+   'O que é Python?',
+   'Python é uma linguagem de programação'
+)
+
+criaCartao(
+'Geografia',
+'Qual a capital da França?'
+'A capital da França é Paris'
+
+)
+criaCartao(
+'Programação',
+'O que é uma função?',
+'uma função é um bloco de código que executa alguma tarefa'
+)
